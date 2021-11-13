338e0e8637ca03b26be148daa021b4ee30cb4b92
Nobeds <info@itbank.lt>
2021-11-13 13:39:35 +0200

    Add project files.

---
+++ Wpf_free_property_management_system/App.xaml.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows;
+
+namespace Wpf_free_property_management_system
+{
+    public partial class App : Application
+    {
+    }
+}
---
+++ Wpf_free_property_management_system/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Wpf_free_property_management_system
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
